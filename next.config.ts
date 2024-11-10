56305cbdf7af0f4f1e2b614317e8d75dd73d2e92
ravinthiranpartheepan1407 <ravinthiranpartheepan1407@gmail.com>
2024-11-10 17:59:03 +0200

    Cors fix

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,5 @@
-import webpack from 'webpack';
-import dotenv from 'dotenv';
-import { NextConfig } from 'next';
-
-// Load environment variables
-dotenv.config();
-
-const nextConfig: NextConfig = {
-    reactStrictMode: true,
-    webpack(config, { isServer }: { isServer: boolean }) {
-        // This allows you to use your environment variables in both the client and server
-        config.plugins.push(
-            new webpack.EnvironmentPlugin(process.env)
-        );
-
-        // Additional customization for Webpack can go here if necessary
-        return config;
-    },
-};
-
-export default nextConfig;
+import type { NextConfig } from 'next'
+ 
+const nextConfig: NextConfig = {}
+ 
+export default nextConfig
